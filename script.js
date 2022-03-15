33c5b2b9df536e5c5c895127c2427d8f3a1d2b57
Jason <Shunji54@users.noreply.github.com>
2022-03-15 16:02:39 +0100

    JS for CLEAR and DEL button + layout changes on calculator

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let numbers = document.querySelectorAll(".number");
+let field = document.querySelector('.screen');
+let clearBtn = document.querySelector('.clearbtn');
+let deleteBtn = document.querySelector('.delbtn')
+
+// Basic operating functions
 function add(x,y) {
     return x+y;
 }
@@ -17,6 +23,30 @@ function divide(x,y) {
     return x/y;
 }
 
+function clearDisplay() {
+    field.innerHTML = '';
+}
+
+function deleteLast() {
+    field.innerHTML = field.innerHTML.slice(0,-1);
+}
+
 function operate(operator,x,y) {
     return operator(x,y);
 }
+
+function display(e) {
+    let screen = document.querySelector('.screen');
+    field.innerHTML += e.dataset.value;
+    return Number(field.innerHTML);
+}
+
+numbers.forEach(button => {
+    button.addEventListener('click', () => {
+        field.innerHTML += button.dataset.value;
+    });
+});
+
+clearBtn.addEventListener('click', clearDisplay);
+deleteBtn.addEventListener('click', deleteLast);
+
