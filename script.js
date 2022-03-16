d730ee310a0ac93f99524b20d8abc67584d15a89
Jason <Shunji54@users.noreply.github.com>
2022-03-16 11:17:06 +0100

    Added JS support for screen number sections

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let numbers = document.querySelectorAll(".number");
-let nbField = document.querySelector('.screen');
+let lowField = document.querySelector('.low-screen');
 let opField = document.querySelector('.op-screen');
+let upField = document.querySelector('.up-screen');
 let clearBtn = document.querySelector('.clearbtn');
-let deleteBtn = document.querySelector('.delbtn')
+let deleteBtn = document.querySelector('.delbtn');
 let operators = document.querySelectorAll('.operator');
 let operatorList = ['+','-','÷','*'];
+let equal = document.querySelector('.equalbtn');
 
 // Basic operating functions
 function add(x,y) {
@@ -27,12 +29,13 @@ function divide(x,y) {
 }
 
 function clearDisplay() {
-    nbField.innerHTML = '';
+    lowField.innerHTML = '';
+    upField.innerHTML = '';
     opField.innerHTML = '';
 }
 
 function deleteLast() {
-    nbField.innerHTML = nbField.innerHTML.slice(0,-1);
+    lowField.innerHTML = lowField.innerHTML.slice(0,-1);
 }
 
 function operate(operator,x,y) {
@@ -41,13 +44,16 @@ function operate(operator,x,y) {
 
 numbers.forEach(button => {
     button.addEventListener('click', () => {
-        nbField.innerHTML += button.dataset.value;
+        lowField.innerHTML += button.dataset.value;
     });
 });
 
 operators.forEach(opBtn => {
     opBtn.addEventListener('click', () => {
         opField.innerHTML = opBtn.dataset.operator;
+        let lastValue = parseInt(lowField.innerHTML);
+        upField.innerHTML += lowField.innerHTML + opField.innerHTML;
+        lowField.innerHTML = '';
     });
 });
 
